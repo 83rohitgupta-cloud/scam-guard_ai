b5ef937185fc9e2b8a53526f0729202104c18623
Lalit Aggarwal <lalit.agarwal.ext@heromotocorp.com>
2026-01-07 08:21:46 +0530

    main file updated

---
+++ main.py
@@ -0,0 +1,15 @@
+#imports
+
+#path normalization
+
+#main function
+def main():
+    """
+    Docstring for main
+    """
+    detector = ScamDetector()
+
+#calls thescam detection workflow - class
+
+if __name__ == "__main__":
+    main()
